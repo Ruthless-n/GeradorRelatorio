46356bba2111a28eef5c12ddaa17ce6570afbc4b
Ruthless-n <ruthcnovais@outlook.com>
2024-03-22 17:03:07 -0300

    Reporting configuration

---
+++ ReportingGenerator/Controllers/NFAppController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ReportingGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NFAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public NFAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public JsonResult GetAll()
+        {
+            string query = "SELECT * FROM NotasFiscais";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+
+        }
+    }
+}
